a67ece1f3259fad722d821a6b8fcb903566e7148
GOWSHIK RAM R <86610098+mysticaven@users.noreply.github.com>
2025-10-23 12:07:46 +0530

    Update Task1.java

--- src/Task1.java
+++ src/Task1.java
@@ -1,44 +1,43 @@
-#include <bits/stdc++.h>
-using namespace std;
+import java.util.*;
 
-
-int main() 
-{
-    int t;cin>>t;
-    while(t--){
-      int n;cin>>n;
-      vector<int>arr(n ,0);
-      for(int i=0;i<n;i++){
-        cin>>arr[i];
-      }
-      
-      int total=0;
-      
-      for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-          int x=0;
-          for(int k=i;k<=j;k++){
-            x^=arr[k];
-          }
-          
-          if(x==0){
-            int pxor=0;int cnt=0;
-            for(int k=i;k<=j;k++){
-              pxor^=arr[k];
-              if(pxor==0)cnt++;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
             }
-            total+=cnt;
-          }
-          
+            
+            int total = 0;
+            
+            for (int i = 0; i < n; i++) {
+                for (int j = i; j < n; j++) {
+                    int x = 0;
+                    for (int k = i; k <= j; k++) {
+                        x ^= arr[k];
+                    }
+                    
+                    if (x == 0) {
+                        int pxor = 0;
+                        int cnt = 0;
+                        for (int k = i; k <= j; k++) {
+                            pxor ^= arr[k];
+                            if (pxor == 0) cnt++;
+                        }
+                        total += cnt;
+                    }
+                }
+            }
+            
+            int ans = (n * (n + 1) * (n + 2)) / 6;
+            System.out.println(ans - total);
         }
-      }
-      
-      int ans= (n*(n+1)*(n+2))/6;
-      cout<<ans-total<<endl;
-      
-      
-      
-      
+        
+        sc.close();
     }
-    return 0;
 }
